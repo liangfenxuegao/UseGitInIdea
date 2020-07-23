d3ccca56e84cbf1e7c838c16a02e2ce78643bc8e
liangfenxuegao <liangfenxuegao@foxmail.com>
2020-07-23 15:56:54 +0800

    development分支认为西瓜好吃

--- xuegao-learn-git-hello/src/main/java/xuegao/learn/git/hello.java
+++ xuegao-learn-git-hello/src/main/java/xuegao/learn/git/hello.java
@@ -5,5 +5,6 @@ public class hello {
         System.out.println("你好Git！");
         System.out.println("更新一");
         System.out.println("创建了development分支");
+        System.out.println("development分支认为西瓜好吃");
     }
 }

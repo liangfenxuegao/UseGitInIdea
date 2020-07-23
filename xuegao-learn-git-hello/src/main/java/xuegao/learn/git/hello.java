92ecbca316debfb44a44f7c043adfb27a31cfd03
liangfenxuegao <liangfenxuegao@foxmail.com>
2020-07-23 15:20:08 +0800

    创建了development分支。

--- xuegao-learn-git-hello/src/main/java/xuegao/learn/git/hello.java
+++ xuegao-learn-git-hello/src/main/java/xuegao/learn/git/hello.java
@@ -4,5 +4,6 @@ public class hello {
     public static void main(String[] args) {
         System.out.println("你好Git！");
         System.out.println("更新一");
+        System.out.println("创建了development分支");
     }
 }
